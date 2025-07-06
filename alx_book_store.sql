5a5baaddff06b72c196d03d626441d7e85328932
yirubal <yiruubal@gmail.com>
2025-07-06 22:33:30 +0300

    add alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE authors (
+CREATE TABLE Authors
+ (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
